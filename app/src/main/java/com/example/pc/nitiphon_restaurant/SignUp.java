b2f4a260eb7241a38012f82a2fcd0e7fb44bd23f
aewooo <Phoso_2538@hotmail.com>
2016-12-03 03:05:23 +0700

    แอพร้านอาหาร

--- app/src/main/java/com/example/pc/nitiphon_restaurant/SignUp.java
+++ app/src/main/java/com/example/pc/nitiphon_restaurant/SignUp.java
@@ -1,15 +1,19 @@
 package com.example.pc.nitiphon_restaurant;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -20,74 +24,85 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
+
+    private EditText userEditText, passwordEditText, nameEditText;
+    private String userString, passwordString, nameString;
+
     private  UserTABLE objUserTABLE;
     private  FoodTABLE objFoodTABLE;
     private  OrderTABLE objOrderTABLE;
 
-    private EditText userEditText,passwordEditText;
-    private  String userString,passwordString;
     private MySQLite mySQLite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        userEditText=(EditText)findViewById(R.id.editText);
-        passwordEditText=(EditText)findViewById(R.id.editText2);
-
+        setContentView(R.layout.activity_sign_up);
         mySQLite=new MySQLite(this);
+        // bindwidget
+        bindWidget();
 
-        connectedSQLite();
-        //Test Add Value
-        //deleteAllData();
-        //testAddValue();
-        synJSONtoSQLite();
-    }
-    public void clickSignInMain(View view){
-        userString =userEditText.getText().toString().trim();
-        passwordString=passwordEditText.getText().toString().trim();
-        if(userString.equals("")||passwordString.equals("")){
-            MyAlert myAlert=new MyAlert();
-            myAlert.myDialog(this,"ช่องว่าง","อย่ามีช่องว่าง");
-        }else{
-            checkUser();
+    } //onCreate
+
+    public void clickSignUp (View view){
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        nameString = nameEditText.getText().toString().trim();
+
+        // check space
+        if (userString.equals("") || passwordString.equals("") || nameString.equals("")){
+            // have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกให้ครบ");
+        }else {
+            // no space
+            updateValueToServer();
         }
-    }
-    public void  clickSignupMain(View view){
-        startActivity(new Intent(MainActivity.this,SignUp.class));
 
-    }
-    private void  checkUser(){
-        try {
-            SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MySQLiteOpenHelper.DATABASE_NAME, MODE_PRIVATE, null);
-            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE WHERE User=" + "'" + userString + "'", null);
-            cursor.moveToFirst();
-            String[] resultStrings = new String[cursor.getColumnCount()];
-            for (int i = 0; i < cursor.getColumnCount(); i++) {
-                resultStrings[i] = cursor.getString(i);
-            }
-            cursor.close();
-            if (passwordString.equals(resultStrings[2])) {
-                Toast.makeText(this, "ยินดีต้อนรับ" + resultStrings[3], Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this,ShopProduct.class );
-                intent.putExtra("Result", resultStrings);
-                startActivity(intent);
-                finish();
-            } else {
-                MyAlert myAlert = new MyAlert();
-                myAlert.myDialog(this, "pass ผิด", "พิมพ์ใหม่");
-            }
-        }catch (Exception e){
-            MyAlert myAlert=new MyAlert();
-            myAlert.myDialog(this,"ไม่มีuser","ไม่มี"+userString+"ในฐานข้อมูล");
-        }
-    }
+    } // clickSignUp
 
+    private void updateValueToServer(){
+        String strURL = "http://www.csclub.ssru.ac.th/s56122201022/csc4202/php_add_user.php";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd","true")
+                .add("User",userString)
+                .add("Password",passwordString)
+                .add("Name",nameString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(strURL).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    connectedSQLite();
+                    synJSONtoSQLite();
+                    finish();
+                }catch (Exception e){
+                    Log.d("Restaurant", "error " + e.toString());
+                }
+
+            }
+        });
+    } //
+
+    private void bindWidget(){
+        userEditText = (EditText) findViewById(R.id.editTextUsername);
+        passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+        nameEditText = (EditText) findViewById(R.id.editTextName);
+    } //bindWidget
     private void synJSONtoSQLite() {
         StrictMode.ThreadPolicy myPolicy =new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(myPolicy);
@@ -95,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         while (intTime<=1){
             InputStream objInputStream = null;
             String strJSON=null;
-            String strUserURL="http://csclub.ssru.ac.th/s56122201022/CSC4202/php_get_userTABLE.php";
+            String strUserURL ="http://csclub.ssru.ac.th/s56122201022/CSC4202/php_get_userTABLE.php";
             String strFoodURL="http://csclub.ssru.ac.th/s56122201022/CSC4202/php_get_foodTABLE.php";
             HttpPost objHttpPost;
             //1.Create InputStream
@@ -155,22 +170,9 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-    private void deleteAllData() {
-        SQLiteDatabase objSqLiteDatabase=openOrCreateDatabase("Restaurant.db",MODE_APPEND,null);
-        objSqLiteDatabase.delete("userTABLE",null,null);
-        objSqLiteDatabase.delete("foodTABLE",null,null);
-        objSqLiteDatabase.delete("orderTABLE",null,null);
-    }
-    private void testAddValue(){
-        //objUserTABLE.addNewUser("testUser","testPass","testName");
-        objFoodTABLE.addNewFood("testFood","testSoure","testPrice");
-        objUserTABLE.addNewUser("BOYZA408","123456","NNN");
-        objOrderTABLE.addNewOrder("dasd","dasd","das","dasd");
-        //objOrderTABLE.addOrder("testOfficer","testDesk","testFood","testItem");
-    }
     private  void connectedSQLite(){
         objUserTABLE=new UserTABLE(this);
         objFoodTABLE=new FoodTABLE(this);
         objOrderTABLE=new OrderTABLE(this);
     }//connectedSQLite
-}   //MainClass
+}
